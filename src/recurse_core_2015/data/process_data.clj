a2462623394a0e9632813509653b086fd8e607a9
Giorgio Leveroni <ppold@Giorgios-MacBook-Pro.local>
2015-12-05 18:37:57 -0500

    Load restaurant data

--- src/recurse_core_2015/data/process_data.clj
+++ src/recurse_core_2015/data/process_data.clj
@@ -2,28 +2,29 @@
   (:require [cheshire.core :as cheshire]
             [clojure.string :refer [join]]))
 
-(def raw-complaints (cheshire/parse-stream (clojure.java.io/reader "resources/dohmh_complaints.json")))
-(def complaints-columns ["DBA" "CUISINE DESCRIPTION" "BUILDING" "STREET" "ZIPCODE"])
+(defmacro defdata [name path]
+  `(def ~name (cheshire/parse-stream (clojure.java.io/reader ~path))))
 
 (defn get-column-names [table]
   (as-> table $
     (get-in $ ["meta" "view" "columns"])
     (map #(get % "name") $)))
 
-(defn filter-by-columns [table column-names]
-  (map #(select-keys (zipmap (get-column-names table) %)
-                     column-names)
-       (get table "data")))
-
-(def filtered-complaints
-  (let [complaints-columns (get-column-names raw-complaints)
-        column-idxs        (map #(.indexOf complaints-columns %) complaints-columns)]
+(defn filter-columns [table required-columns]
+  (let [column-names (get-column-names table)
+        column-idxs  (map #(.indexOf column-names %) required-columns)]
     (sequence (map #(map (partial nth %) column-idxs))
-              (get raw-complaints "data"))))
+              (get table "data"))))
 
-(def unique-complaints (set filtered-complaints))
+(defdata raw-restaurants "resources/DOHMH_Restaurant.json")
+(def restaurants-columns ["Incident Address" "Incident Zip" "Latitude" "Longitude"])
+(def restaurants (map (partial zipmap [:address :zip :latitude :longitude])
+                      (set (filter-columns raw-restaurants restaurants-columns))))
+
+(defdata raw-complaints "resources/dohmh_complaints.json")
+(def complaints-columns ["DBA" "CUISINE DESCRIPTION" "STREET" "BUILDING" "ZIPCODE"])
 (def complaints (map #(hash-map :title (nth % 0)
-                               :cuisine (nth % 1)
-                               :address (join " " (map (partial nth %) [3 2]))
-                               :zip (nth % 4))
-                     unique-complaints))
+                                :cuisine (nth % 1)
+                                :address (join " " (map (partial nth %) [2 3]))
+                                :zip (nth % 4))
+                     (set (filter-columns raw-complaints complaints-columns))))
